494aa2c576dc90f5c38394390766d2ad79b7991c
HeoSeokJun <gjtjrl303@naver.com>
2025-12-23 12:54:45 +0900

    Add files via upload

---
+++ src/main/java/com/example/gitryeokoffice/GitryeokofficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitryeokoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitryeokofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitryeokofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitryeokoffice/GitryeokofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitryeokoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitryeokofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
